8578ec007aa635717e4b497fb27a625eadbeb005
hiennq99 <hien124ytbg@gmail.com>
2021-03-14 22:35:24 +0700

    add env

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,12 @@
 const { createPool } = require('mysql');
+require('dotenv').config();
 
 const pool = createPool({
     port: 3306,
-    host: 'localhost',
-    user: 'admin',
-    password: '123',
-    database: 'chatbot',
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DA_NAME,
     connectionLimit: 10
 });
 
